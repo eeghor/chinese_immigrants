ec08bce49eba7a688817bd5a081c0e4841b14972
Igor Korostil <eeghor@gmail.com>
2017-08-15 14:08:05 +1000

    added saving to file

--- chinese_immigrants.py
+++ chinese_immigrants.py
@@ -2,11 +2,14 @@
 #import selenium
 import string
 import itertools
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import sys
+from random import randint
+import time
 # from selenium.webdriver.common.keys import Keys
 # import time
 # import sys
@@ -30,6 +33,16 @@ def get_names_from_page():
 				continue
 	return names_on_this_page 
 
+def another_page():
+
+	try:
+		driver.find_element_by_id("nextPage")
+		another_page_flag = True
+	except NoSuchElementException:
+		another_page_flag = False
+
+	return another_page_flag
+
 # full path to the webdriver to use; use webdriver.PhantomJS() for invisible browsing
 driver = webdriver.Chrome('/Users/ik/Codes/chinese_immigrants/webdriver/chromedriver')
 
@@ -46,9 +59,8 @@ for pair in itertools.product(string.ascii_lowercase, repeat=2):   # 'abcdefghij
 	enter_name_input = driver.find_element_by_xpath("//input[contains(@title,'Enter') and contains(@title, 'Name')]")
 	enter_name_input.clear()
 	enter_name_input.send_keys("".join(pair) + "*")
-	# find search button
-	search_button = driver.find_element_by_xpath("//input[@type='submit']")
-	search_button.click()
+	# find and click the search button
+	driver.find_element_by_xpath("//input[@type='submit']").click()
 	# wait until the results show up
 	how_many_found = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "search_term_value")))
 	
@@ -60,22 +72,27 @@ for pair in itertools.product(string.ascii_lowercase, repeat=2):   # 'abcdefghij
 			print("found nothing, moving on...")
 			continue
 	except:
+
 		print("ERROR: didn\'t get any search results at all. exiting..")
 		sys.exit(0)
 	
 	collected_names.update(get_names_from_page())
 	# see if there's another page
-	try:
-		next_page = driver.find_element_by_id("nextPage")
-		next_page.click()
+
+	while another_page():
+
+		driver.find_element_by_id("nextPage").click()
 		WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.ID, "previousPage")))
 		collected_names.update(get_names_from_page())
-		print("collected names so far: {}".format(len(collected_names)))
-	
-	except:
-		# no next page
-		print("no more pages")
-		continue
+
+	time.sleep(randint(1,5))
+	continue  # move on to the next letter triple
+
+# save the collected names to a file
+with open("chinese_immigrant_names.txt", "w") as f:
+	for name in collected_names:
+		f.write("{}\n".format(name))
+print("collected a list of {} unique names".format(len(collected_names)))
 	
 	
 	
